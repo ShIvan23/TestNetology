9bcdbb66d7a66e4ae8bcd59e4ee02c72adc08989
Ivan Shishkin <ivan23-1@yandex.ru>
2022-04-01 16:46:21 +0300

    First commit

---
+++ Навигация в iOS и  жизненный цикл UIViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Навигация в iOS и  жизненный цикл UIViewController
+//
+//  Created by Ivan on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func push() {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        storyboard.instantiateViewController(withIdentifier: "SecondVC")
+    }
+}
+
+/// Ответы на вопросы
+/// Да, может быть несколько таргетов. Например приложение для такси. Один интерфейс для клиентов, второй для водителей. Например таргет с тестами, для виджетов
+/// Connection Inspector - для связи элементов в билдере с кодом
